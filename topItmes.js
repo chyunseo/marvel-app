e4c2dd7031e66b71c4f4a17308bcd8b77d2b04a8
hyunseo <leehs430@naver.com>
2022-01-23 00:09:32 +0900

    first commit

---
+++ topItmes.js
@@ -0,0 +1,49 @@
+const captain = document.querySelector(".captain");
+const iron = document.querySelector(".iron");
+const thor = document.querySelector(".thor");
+const hulk = document.querySelector(".hulk");
+const widow = document.querySelector(".widow");
+const ant = document.querySelector(".ant");
+const panther = document.querySelector(".panther");
+const spyder = document.querySelector(".spyder");
+
+captain.addEventListener("click", onCaptain);
+iron.addEventListener("click", onIron);
+thor.addEventListener("click", onThor);
+hulk.addEventListener("click", onHulk);
+widow.addEventListener("click", onWidow);
+ant.addEventListener("click", onAnt);
+panther.addEventListener("click", onPanther);
+spyder.addEventListener("click", onSpyder);
+
+function onCaptain() {
+  alert("hello");
+}
+
+function onIron() {
+  alert("hello");
+}
+
+function onThor() {
+  alert("hello");
+}
+
+function onHulk() {
+  alert("hello");
+}
+
+function onWidow() {
+  alert("hello");
+}
+
+function onAnt() {
+  alert("hello");
+}
+
+function onPanther() {
+  alert("hello");
+}
+
+function onSpyder() {
+  alert("hello");
+}
