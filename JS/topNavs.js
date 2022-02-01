625a384a1a2dc45a2c0101d75bb3dc387c9d6bff
hyunseo <leehs430@naver.com>
2022-02-01 23:25:12 +0900

    2022_02_03_9:30am

--- JS/topNavs.js
+++ JS/topNavs.js
@@ -1,9 +1,22 @@
 const home = document.querySelector("#home");
 const Home = document.querySelector("#Home");
+const movieList = document.querySelector("#movieList");
+const characters = document.querySelector("#characters");
+const recommand = document.querySelector("#recommand");
+const games = document.querySelector("#games");
 
-function onHome() {
+function onHomes() {
   window.open("https://www.google.com/");
 }
 
-home.addEventListener("click", onHome);
-Home.addEventListener("click", onHome);
+function onMovieList() {} 
+function onCharacters() {}
+function onRecommand() {}
+function onGames() {}
+
+home.addEventListener("click", onHomes);
+Home.addEventListener("click", onHomes);
+movieList.addEventListener("click", onMovieList);
+characters.addEventListener("click", onCharacters);
+recommand.addEventListener("click", onRecommand);
+games.addEventListener("click", onGames);
